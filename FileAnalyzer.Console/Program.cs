14b54cc1bc7ed42ea7c46db89880b7bcfbd6685b
Tavershima <tatarprofessional@gmail.com>
2022-05-02 00:52:20 +0100

    reset the stream seek position after each file check

---
+++ FileAnalyzer.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using Latch.FileAnalyzer;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.IO;
+
+namespace FileAnalyzer.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.EnumerateFiles(SpecialDirectories.MyPictures);
+            foreach (var item in files)
+            {
+                var s = new StreamReader(item);
+                bool r = FileChecker.GetFileTypeExtension(s.BaseStream, out string ext);
+                Console.WriteLine($"{Path.GetFileName(item)}: {r}");
+            }
+        }
+    }
+}
